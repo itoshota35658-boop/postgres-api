f356bf2cd3fa6626d43313013b155b92729cd484
itoshota35658-boop <ito.shota35658@gmail.com>
2026-04-21 00:47:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "test_db",
-  password: "Shota35658",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 app.get("/employees", async (req, res) => {
